1f42f82f1191b3098befbbbf5700d3f2c6d10744
taka826 <on85.ohno.takamasa@gmail.com>
2020-11-07 18:35:15 +0900

    ログイン機能実装

--- app/controllers/railways_controller.rb
+++ app/controllers/railways_controller.rb
@@ -1,6 +1,15 @@
 class RailwaysController < ApplicationController
+  before_action :move_to_index, except: :index
 
   def index
   end
-  
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
