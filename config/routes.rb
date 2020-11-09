3234ae1d38936b9a04045f653ae2852d9b5e897a
taka826 <on85.ohno.takamasa@gmail.com>
2020-11-09 21:43:26 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'quizs/index'
   devise_for :users
   root to: 'railways#index'
-  resources :railways, only: [:index, :new, :create, :destroy, :edit]
+  resources :railways, only: [:index, :new, :create, :destroy, :edit, :update]
   resources :quizs, only: :index
 end
